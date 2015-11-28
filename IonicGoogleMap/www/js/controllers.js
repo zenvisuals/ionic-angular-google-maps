ca46618ec8a020a3698b4fe7ac1b8c10035aa9b7
Chee Zhiquan <zhiquan@chees-imac.gateway.2wire.net>
2015-11-28 13:29:13 +0800

    updated README.md

--- IonicGoogleMap/www/js/controllers.js
+++ IonicGoogleMap/www/js/controllers.js
@@ -50,7 +50,7 @@ angular.module('starter.controllers', [])
 .controller('BrowseCtrl', function($scope, $ionicSideMenuDelegate, $cordovaGeolocation){
 
     $ionicSideMenuDelegate.canDragContent(false);
-    $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 8 };
+    $scope.map = {center: {latitude: 60.1451, longitude: -99.6680 }, zoom: 8 };
     $scope.options = {scrollwheel: true};
     $scope.markericon = "img/moose.png";
     $scope.markers = []
@@ -58,7 +58,6 @@ angular.module('starter.controllers', [])
     $cordovaGeolocation
       .getCurrentPosition()
       .then(function (position) {
-        console.log(position);
         var lat  = position.coords.latitude
         var long = position.coords.longitude
         $scope.map = {center: {latitude: lat, longitude: long}, zoom: 16 };
